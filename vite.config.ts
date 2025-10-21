c05329f760db7e5840938f910303d2242eae518c
Denzell Griffith <grift210@gmail.com>
2025-10-20 21:28:38 -0600

    chore: config absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +11,16 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    tailwindcss(),
   ],
-})
+  server: {
+    port: 4000,
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@root': path.resolve(__dirname),
+    },
+  },
+});
