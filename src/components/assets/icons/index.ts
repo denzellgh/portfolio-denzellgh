ba237c063572f4bb8e0f060c84aaf0d0fb40f8c6
Denzell Griffith <grift210@gmail.com>
2025-10-27 23:50:56 -0600

    add: needed icons for sonner component

--- src/components/assets/icons/index.ts
+++ src/components/assets/icons/index.ts
@@ -1,1 +1,8 @@
-export { IconLogo } from '@/components/assets/icons/icon-logo';
+export { IconLogo } from './icon-logo';
+export { IconGithub } from './github';
+export { IconLinkedin } from './linkedin';
+export { CircleCheckIcon } from './circleCheckIcon'
+export { InfoIcon } from './infoIcon'
+export { Loader2Icon } from './loader2Icon'
+export { OctagonXIcon } from './ocatonXIcon'
+export { TriangleAlertIcon } from './triangleAlertIcon'
